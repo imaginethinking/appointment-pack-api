49e3ba64445ecf6e9e83ad21b469e40de4f2c4ee
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-06-29 19:42:02 +0100

    feat(project): Set-up initial Spring Boot project with Maven as dependency manager

---
+++ src/main/java/net/imaginethinking/appointmentpack/AppointmentPackApiApplication.java
@@ -0,0 +1,13 @@
+package net.imaginethinking.appointmentpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentPackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentPackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/imaginethinking/appointmentpack/AppointmentPackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.imaginethinking.appointmentpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentPackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
