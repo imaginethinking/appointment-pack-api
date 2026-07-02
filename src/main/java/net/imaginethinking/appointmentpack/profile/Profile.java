380cff2b40c656086956d66f17e636d549de525d
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-07-02 17:12:03 +0100

    refactor(profile): remove redundant referencedColumnName in user field

--- src/main/java/net/imaginethinking/appointmentpack/profile/Profile.java
+++ src/main/java/net/imaginethinking/appointmentpack/profile/Profile.java
@@ -16,7 +16,7 @@ import java.time.LocalDate;
 public class Profile extends BaseEntity {
 
     @OneToOne()
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JoinColumn(name = "user_id")
     private User user;
 
     @Column(name = "first_name", nullable = false)
