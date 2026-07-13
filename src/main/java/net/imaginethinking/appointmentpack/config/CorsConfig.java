d9445407d8f05a3e8e44362076b31402bbc26c41
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-07-13 21:31:03 +0100

    fix(security): update cors configuration and security chain to allow requests from localhost

--- src/main/java/net/imaginethinking/appointmentpack/config/CorsConfig.java
+++ src/main/java/net/imaginethinking/appointmentpack/config/CorsConfig.java
@@ -1,24 +1,24 @@
 package net.imaginethinking.appointmentpack.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                        .allowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "PATCH",
+                        "DELETE"
+                )
+                .allowedHeaders("*")
+                .maxAge(3600);
     }
 }
