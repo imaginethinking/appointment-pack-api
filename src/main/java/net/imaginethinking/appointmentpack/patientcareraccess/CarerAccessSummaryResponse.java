1542b60baf964bf4e96a14910daad061ded73a3b
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-07-22 04:05:07 +0100

    fix(patientcareraccess): fix field spelling error in response DTO

--- src/main/java/net/imaginethinking/appointmentpack/patientcareraccess/CarerAccessSummaryResponse.java
+++ src/main/java/net/imaginethinking/appointmentpack/patientcareraccess/CarerAccessSummaryResponse.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 public record CarerAccessSummaryResponse(
         UUID userId,
         UUID profileId,
-        String firstname,
+        String firstName,
         String lastName,
         String email
 ) {
