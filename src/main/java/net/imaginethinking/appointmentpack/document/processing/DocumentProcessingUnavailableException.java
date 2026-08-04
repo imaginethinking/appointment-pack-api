f4208218134dac49d755dff6932c2d838e0a9ee7
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-08-05 00:30:59 +0100

    chore(document-processing): add exception for offline document processing service

---
+++ src/main/java/net/imaginethinking/appointmentpack/document/processing/DocumentProcessingUnavailableException.java
@@ -0,0 +1,12 @@
+package net.imaginethinking.appointmentpack.document.processing;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE, reason = "Document processing service is unavailable")
+public class DocumentProcessingUnavailableException extends RuntimeException {
+
+    public DocumentProcessingUnavailableException(Throwable cause) {
+        super("Document processing service is unavailable", cause);
+    }
+}
