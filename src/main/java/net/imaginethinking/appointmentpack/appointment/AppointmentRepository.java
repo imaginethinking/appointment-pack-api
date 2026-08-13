f499d3361c8cfb58cb3391882abef84ad6cb5eb6
ImagineThinking <104801799+ElotherYT@users.noreply.github.com>
2026-08-13 20:32:12 +0100

    refactor(appointment): fix formatting

--- src/main/java/net/imaginethinking/appointmentpack/appointment/AppointmentRepository.java
+++ src/main/java/net/imaginethinking/appointmentpack/appointment/AppointmentRepository.java
@@ -5,8 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
-public interface AppointmentRepository
-        extends JpaRepository<Appointment, UUID> {
+public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
 
     boolean existsBySourceDocument_Id(UUID sourceDocumentId);
 
